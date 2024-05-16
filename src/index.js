bb007e77aed652b8324b28418eef28ef3f036f62
J Vasilev <52400967+jason-vasilev@users.noreply.github.com>
2024-05-16 22:47:22 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import "./styles.scss";
+
+// Or if installed from NPM to use with a bundler
+import Artyom from "artyom.js";
+// const artyom = require("artyom.js");
+const artyom = new Artyom();
+
+function isgetReplyAvailable() {
+  return typeof getReply !== "undefined" && typeof getReply === "function";
+}
+
+if (isgetReplyAvailable()) {
+  let command;
+  let timeoutId;
+  let setIntervalTimer;
+
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    button.innerHTML = "Talk now 🙂";
+    setIntervalTimer = setInterval(() => {
+      const randomNumber = Math.floor(Math.random() * 6) + 2;
+      if (randomNumber % 2 === 0) {
+        button.innerHTML = "Talk now 😮";
+      } else {
+        button.innerHTML = "Talk now 🙂";
+      }
+    }, 100);
+    clearTimeout(timeoutId);
+
+    command = "";
+    timeoutId = setTimeout(() => {
+      clearInterval(setIntervalTimer);
+      const response = getReply(command);
+
+      artyom.say(response);
+
+      button.innerHTML = "Give a new command";
+    }, 5000);
+  });
+
+  var UserDictation = artyom.newDictation({
+    continuous: false, // Enable continuous if HTTPS connection
+    onResult: function (text) {
+      // Do something with the text
+      if (text.length > command.length) {
+        command = text;
+        console.log(command);
+      }
+    },
+    onStart: function () {
+      console.log("Dictations started by the users");
+    },
+    onEnd: function () {
+      console.log("Dictation stopped by the user");
+    },
+  });
+
+  UserDictation.start();
+} else {
+  alert("add the getReply function!");
+}
