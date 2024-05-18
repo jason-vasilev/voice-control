24f993031473af3631109855faab85bf5a39ecc1
J Vasilev <52400967+jason-vasilev@users.noreply.github.com>
2024-05-18 13:54:07 +0200

    Testing voice control

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ if (isgetReplyAvailable()) {
     timeoutId = setTimeout(() => {
       clearInterval(setIntervalTimer);
       const response = getReply(command);
-
+      console.log("response: ", response);
       artyom.say(response);
 
       button.innerHTML = "Give a new command";
@@ -63,83 +63,87 @@ if (isgetReplyAvailable()) {
 // trainee code from here below
 
 function getReply(command) {
-  if (command === str1) {
-    const userName = saveName(str1);
-    return `Nice to meet you ${userName}`;
-  } else if (command === str2) {
+  let result;
+  if (command.includes("hello my name is")) {
+    const userName = saveName(command);
+    result = `nice to meet you ${userName}`;
+  } else if (command.includes(str2)) {
     if (!str1) {
-      return `you have not tell your name yet`;
+      result = `you have not tell your name yet`;
     }
     const yourName = saveName(str1);
-    return `Your name is ${yourName}`;
-  } else if (command === str3) {
-    return addActivity(str3);
-  } else if (command === str4) {
-    return addActivity(str4);
-  } else if (command === str5) {
-    return removeActivity(str5);
-  } else if (command === str6) {
-    return `You have ${todo.length} todos - ${todo.join(" , ")}`;
-  } else if (command === str7) {
-    return new Date().toLocaleDateString("en-US", {
+    result = `Your name is ${yourName}`;
+  } else if (command.includes("add")) {
+    const activity = addActivity(command);
+    result = `${activity} added to your todo`;
+  } else if (command.includes("remove")) {
+    result = removeActivity(str5);
+  } else if (command.includes("what is on my")) {
+    const length = `You have ${listArr.length} todos - ${listArr.join(" , ")}`;
+    result = length;
+  } else if (command.includes(str7)) {
+    result = new Date().toLocaleDateString("en-US", {
       day: "numeric",
       month: "long",
       year: "numeric",
     });
-  } else if (command === str8) {
-    return calcOperations(str8);
-  } else if (command === str9) {
+  } else if (command.startsWith("what is")) {
+    return calcOperations(command);
+  } else if (command.includes(str9)) {
     setTimeout(timer, 4000);
-    return "Timer set for 4 seconds";
-  } else if (command === str10) {
-    return guessAboutToday(str10);
+    result = "Timer set for 4 seconds";
+  } else if (command.includes("how will")) {
+    result = guessAboutToday();
   } else {
-    return "error";
+    result = "error";
   }
+  return result;
 }
 function saveName(string) {
-  stringArray = string.split(" ");
+  const stringArray = string.split(" ");
   const userName = stringArray[stringArray.length - 1];
   return userName;
 }
 function addActivity(string) {
   const startIndex = string.indexOf("add") + 4;
-  const endIndex = string.indexOf("to my todo");
+  const endIndex = string.indexOf("to my");
   const activity = string.substring(startIndex, endIndex).trim();
-  todo.push(activity);
-  return `${activity} added to your todo`;
+  listArr.push(activity);
+  return activity;
 }
+
 function removeActivity(string) {
   const startIndex = string.indexOf("remove") + 7;
-  const endIndex = string.indexOf("from my todo");
+  const endIndex = string.indexOf("from my");
   const activity = string.substring(startIndex, endIndex).trim();
-  const index = todo.indexOf(activity);
-  todo.splice(index, 1);
+  const index = listArr.indexOf(activity);
+  listArr.splice(index, 1);
+
   return `Removed ${activity} from your todo`;
 }
 function calcOperations(string) {
-  stringArray = string.split(" ");
+  let output;
+  const stringArray = string.split(" ");
   const number1 = Number(stringArray[2]);
   const number2 = Number(stringArray[4]);
   const operation = stringArray[3];
-
-  switch (operation) {
-    case "+":
-      return number1 + number2;
-    case "-":
-      return number1 - number2;
-    case "*":
-      return number1 * number2;
-    case "/":
-      return number1 / number2;
-    default:
-      return "Please give proper input";
+  if (operation.includes("+")) {
+    output = number1 + number2;
+  } else if (operation.includes("-")) {
+    output = number1 - number2;
+  } else if (operation.includes("*")) {
+    output = number1 * number2;
+  } else if (operation.includes("/")) {
+    output = number1 / number2;
+  } else {
+    output = "Please give proper input";
   }
+  return output.toString();
 }
 function timer() {
   console.log("Timer Done");
 }
-function guessAboutToday(string) {
+function guessAboutToday() {
   let todayLuck = "";
   const words = {
     positiveWords: ["great", "awesome", "lucky"],
@@ -158,29 +162,27 @@ function guessAboutToday(string) {
   return todayLuck;
 }
 
-//let str1; // used when the person not mentioned name yet
-let todo = [];
-const str1 = "Hello my name is Benjamin Benjamin";
-const str2 = "What is my name?";
-const str3 = "Add fishing to my todo";
-const str4 = "Add singing in the shower to my todo";
-const str5 = "Remove fishing from my todo";
-const str6 = "What is on my todo?";
-const str7 = "What day is it today?";
-const str8 = "what is 4 * 12";
-const str9 = "Set a timer for 4 seconds";
-const str10 = "How will the day be for me?";
+let listArr = [];
+//let str1;
+const str1 = "hello my name is bob";
+const str2 = "what is my name";
+const str3 = "add fishing to my to-do";
+const str4 = "add singing in the shower to my to-do";
+const str5 = "remove fishing from my todo";
+const str6 = "what is on my todo";
+const str7 = "what day is it today";
+const str8 = "what is 3 / 3";
+const str9 = "set timer for 4 seconds";
+const str10 = "how will the day be for me";
 
-console.log(getReply(str1));
-console.log(getReply(str2));
-console.log(getReply(str3));
-console.log(todo);
-console.log(getReply(str4));
-console.log(todo);
-console.log(getReply(str5));
-console.log(todo);
-console.log(getReply(str6));
-console.log(getReply(str7));
-console.log(getReply(str8));
-console.log(getReply(str9));
-console.log(getReply(str10));
+// console.log(getReply(str1));
+// console.log(getReply(str2));
+// console.log(getReply(str3));
+// console.log(listArr);
+// console.log(getReply(str4));
+// console.log(getReply(str5));
+// console.log(getReply(str6));
+// console.log(getReply(str7));
+// console.log(getReply(str8));
+// console.log(getReply(str9));
+// console.log(getReply(str10));
